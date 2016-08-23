5a0e26c71b2e748b2e8df78b9662e31ab3e05140
nowis <tannai.simon@gmail.com>
2016-08-23 11:49:10 +0200

    Fix errors, fix testjson route missing, explore more datas from databases.json

--- app/scripts/controllers/faq.js
+++ app/scripts/controllers/faq.js
@@ -9,19 +9,18 @@
  */
 angular.module('odaChallengeApp')
   .controller('FaqCtrl', ['$scope','$http', function ($scope, $http) {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
 
     $scope.$on('$viewContentLoaded', function(event) {
-      $http.get('http://127.0.0.1:6969/faq.json').then(function successCallback(response) {
-          console.log(response.data)
+      event.preventDefault();
+
+      $http.get('http://127.0.0.1:6969/faq.json').then(
+        function (response) {
+          console.log(response.data);
            $scope.questionList = response.data;
            //console.log($scope.questionList)
-        }, function errorCallback(response){
-
+        },
+        function (response) {
+          console.log(response);
         });
     });
 
--- app/scripts/controllers/informationrequest.js
+++ app/scripts/controllers/informationrequest.js
@@ -11,36 +11,40 @@ angular.module('odaChallengeApp')
   .controller('InformationrequestCtrl', ['$scope','$http', function ($scope, $http) {
 
   	$scope.$on('$viewContentLoaded', function(event) {
-      $http.get('http://localhost:8000/DataTest/phone.json').then(function successCallback(response) {
+      event.preventDefault();
+      $http.get('http://localhost:8000/DataTest/phone.json').then(
+        function (response) {
            $scope.phone = response.data.phone;
-           console.log($scope.phone)
-        }, function errorCallback(response){
-
+           console.log($scope.phone);
+        },
+        function (response) {
+          console.log(response);
         });
     });
 
     $scope.reqInfo = {};
 
-    $scope.sendQuestion = function(reqInfo){
+    $scope.sendQuestion = function(reqInfo) {
 
-	    $scope.errorSubject = ""
-		$scope.errorMessage = ""
+	    $scope.errorSubject = '';
+		$scope.errorMessage = '';
 
       var check = true;
 
-      if(!reqInfo.subject || reqInfo.subject == ""){
-        $scope.errorSubject = "Merci de renseigner un sujet";
-        check = false
+      if(!reqInfo.subject || reqInfo.subject === '') {
+        $scope.errorSubject = 'Merci de renseigner un sujet';
+        check = false;
       }
 
-      if(!reqInfo.message || reqInfo.message == ""){
-        $scope.errorMessage = "Merci d'indiquer votre problème";
-        check = false
+      if(!reqInfo.message || reqInfo.message === '') {
+        $scope.errorMessage = 'Merci d\'indiquer votre problème';
+        check = false;
       }
 
-      if(check == true)
-        $http.post('http://localhost:8000/supportContact', reqInfo)
-    	
+      if(check === true) {
+        $http.post('http://localhost:8000/supportContact', reqInfo);
+      }
+
     };
 
   }]);
